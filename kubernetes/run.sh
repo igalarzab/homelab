3019c04d705227c0c5d276d0cf57cb5584506f8e
Jose Galarza <igalarzab@gmail.com>
2024-08-01 23:41:34 +0800

    Moving transmission

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-helmsman \
-  --apply \
-  --subst-env-values \
-  -f helmsman.yml
+while getopts "hdr" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Usage: $0 [-d|--dry-run] [-r|--run]"
+        exit 0
+        ;;
+    d)
+      helmsman \
+        --dry-run \
+        --subst-env-values \
+        -f helmsman.yml
+        ;;
+    r)
+      helmsman \
+        --apply \
+        --subst-env-values \
+        -f helmsman.yml
+        ;;
+    esac
+done
