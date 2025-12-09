1ab07235e344ca8e36baf630509fc850726c09eb
Jose Galarza <igalarzab@gmail.com>
2025-12-09 23:26:34 +0800

    Harden server

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,9 +2,25 @@
 
 REMOTE_USER="${REMOTE_USER:-$USER}"
 
+# Parse command line arguments
+if [ -z "$1" ]; then
+    echo "Error: Playbook parameter is required"
+    echo "Usage: ./run.sh [jupyter|saturn]"
+    exit 1
+fi
+
+if [ "$1" == "jupyter" ]; then
+    PLAYBOOK="jupyter.yml"
+elif [ "$1" == "saturn" ]; then
+    PLAYBOOK="saturn.yml"
+else
+    echo "Error: Invalid playbook '$1'"
+    exit 1
+fi
+
 ansible-playbook \
     --ask-become-pass \
-    --inventory-file hosts.ini \
+    --inventory hosts.ini \
     --user "$REMOTE_USER" \
     --extra-vars "@vars.yaml" \
-    server.yml
+    "$PLAYBOOK"
